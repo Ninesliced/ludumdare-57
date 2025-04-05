ed02417d787a904ea9b068d8a73e073e006097b2
notgoyome <guillaume1.tran@epitech.eu>
2025-04-05 23:39:24 +0200

    ADDED particle

--- scenes/entities/player/components/player_burrowed_component.gd
+++ scenes/entities/player/components/player_burrowed_component.gd
@@ -17,13 +17,13 @@ func physics_process(delta: float) -> void:
 	if input_vector != Vector2.ZERO:
 		last_input_vector = input_vector
 
-	current_direction = current_direction.move_toward(last_input_vector, acceleration * delta)
+	# current_direction = current_direction.move_toward(last_input_vector, acceleration * delta)
 		# current_direction = input_vector.normalized()
 
-	# var angle = rotate_toward(current_direction.angle(), last_input_vector.angle(),rotation_speed * delta)
-	# current_direction = Vector2(cos(angle), sin(angle)).normalized()
-	# if current_direction != Vector2.ZERO:
-	# 	current_direction = current_direction.normalized()
+	var angle = rotate_toward(current_direction.angle(), last_input_vector.angle(),rotation_speed * delta)
+	current_direction = Vector2(cos(angle), sin(angle)).normalized()
+	if current_direction != Vector2.ZERO:
+		current_direction = current_direction.normalized()
 	velocity = velocity.move_toward(current_direction * move_speed, acceleration * delta)
 
 	player.velocity = velocity
