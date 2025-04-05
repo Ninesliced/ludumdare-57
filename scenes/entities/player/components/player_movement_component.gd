3e93c03731f559b5330ab7919b3a97bf27c11b60
notgoyome <guillaume1.tran@epitech.eu>
2025-04-05 14:45:11 +0200

    ADD statemacine and ground/borrow state to player

--- scenes/entities/player/components/player_movement_component.gd
+++ scenes/entities/player/components/player_movement_component.gd
@@ -1,6 +1,10 @@
 extends Node
 
+class_name PlayerGroundMovement
+
 @export var player : Player = null
+
+var _enable_movement: bool = true
 
 @export var move_speed: float = 80.0
 @export var acceleration: float = 80 * 12
@@ -9,12 +13,6 @@ extends Node
 @export var jump_height: float = 24.0
 @export var jump_time_to_peak: float = 0.4
 @export var jump_time_to_descent: float = 0.3
-
-
-var jump_requested: bool = false
-var coyote_timer: Timer = null
-@export var coyote_time: float = 0.1
-
 
 @onready var jump_velocity: float = -(2.0 * jump_height) / jump_time_to_peak 
 @onready var jump_gravity: float = (-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)
@@ -25,13 +23,14 @@ var coyote_timer: Timer = null
 var gravity_enabled: bool = true
 
 func _ready() -> void:
-	coyote_timer = Timer.new()
-	coyote_timer.wait_time = coyote_time
-	coyote_timer.one_shot = true
-	coyote_timer.timeout.connect(disable_coyote)
-	add_child(coyote_timer)
+	pass
 
-func _physics_process(delta: float) -> void:
+func set_enable_movement(enable: bool) -> void:
+	_enable_movement = enable
+
+func physics_process(delta: float) -> void:
+	if not _enable_movement:
+		return
 	var velocity = player.velocity
 
 	
@@ -40,36 +39,27 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= get_gravity(velocity) * delta
 
 	if Input.is_action_just_pressed("jump"):
-		request_jump()
-	if jump_requested and player.is_on_floor():
+		player.request_jump()
+	if player.jump_requested and player.is_on_floor():
 		velocity.y = jump(velocity)
 
 	if Input.is_action_just_released("jump") and velocity.y < 0.0:
 		velocity.y = velocity.y * 0.5
 
 	player.velocity = velocity
-	
+
+
+
 
 func get_gravity(velocity) -> float:
 	return jump_gravity if velocity.y < 0.0 else fall_gravity
 
 ## JUMP functionnality
 
-func request_jump() -> void:
-	jump_requested = true
-	if not coyote_timer.is_stopped():
-		coyote_timer.start()
-	else:
-		coyote_timer.stop()
-		coyote_timer.start()
-
 func jump(velocity: Vector2) -> float:
 	velocity.y = jump_velocity
 	return velocity.y
 
-func disable_coyote():
-	coyote_timer.stop()
-	jump_requested = false
 	
 
 func handle_movement(velocity: Vector2, delta: float) -> float:
--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,7 +1,16 @@
 extends CharacterBody2D
 class_name Player
 
+var jump_requested: bool = false
+var coyote_timer: Timer = null
+@export var coyote_time: float = 0.1
+
 func _ready():
+    coyote_timer = Timer.new()
+    coyote_timer.wait_time = coyote_time
+    coyote_timer.one_shot = true
+    coyote_timer.timeout.connect(disable_coyote)
+    add_child(coyote_timer)
     pass
 
 func _process(delta):
@@ -10,3 +19,16 @@ func _process(delta):
 func _physics_process(delta):
     move_and_slide()
     pass
+
+
+func request_jump() -> void:
+    jump_requested = true
+    if not coyote_timer.is_stopped():
+        coyote_timer.start()
+    else:
+        coyote_timer.stop()
+        coyote_timer.start()
+
+func disable_coyote():
+    coyote_timer.stop()
+    jump_requested = false
