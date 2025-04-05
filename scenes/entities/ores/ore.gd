e1e297192fd136fd7841d4e27980296733728f73
notgoyome <guillaume1.tran@epitech.eu>
2025-04-05 18:18:08 +0200

    ADDED particles to ruby

--- scenes/entities/ores/ore.gd
+++ scenes/entities/ores/ore.gd
@@ -2,8 +2,17 @@ extends Node2D
 
 class_name Ore
 
-
+var collected : bool = false
+@onready var destroyParticle : CPUParticles2D = %DestroyParticle
+@onready var sprite: AnimatedSprite2D = %Sprite2D
 
 func _on_area_2d_body_entered(body:Node2D) -> void:
-    queue_free()
-    pass # Replace with function body.
+	if collected:
+		return
+	collected = true
+	
+	sprite.hide()
+	destroyParticle.emitting = true
+	await destroyParticle.finished
+	queue_free()
+	pass # Replace with function body.
