ad843234c68a6a201e89213569eb99d413ecabbe
ARKANYOTA <arkanyota@icloud.com>
2025-04-05 16:29:51 +0200

    get online

--- scenes/map/map_loader.gd
+++ scenes/map/map_loader.gd
@@ -12,14 +12,6 @@ func load_new_layer():
 	var layerA: Node2D = layerA_ressources.instantiate()
 	var tileMapLayer: TileMapLayer = layerA.get_node("Ores")
 	layerA.position.y = current_depth*16
-	for pixelx in range(0, MAP_SIZE_X):
-		for pixely in range(0, MAP_SIZE_Y):
-			var current_pixel = tileMapLayer.get_cell_source_id(Vector2i(pixelx, pixely))
-			var current_pixel2 = tileMapLayer.get_cell_tile_data(Vector2i(pixelx, pixely))
-			print(current_pixel2)
-			if(is_instance_of(current_pixel2, TileData)):
-				print("\t", str(current_pixel2.get_property_list()))
-		print("")
 	current_depth += MAP_SIZE_Y
 	add_child(layerA)
 	
