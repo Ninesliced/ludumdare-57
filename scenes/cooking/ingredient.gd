d32f1901cc54f1d72326d35aa0b1a5920c46cdbc
Yolwoocle <leo.bernard2015@hotmail.com>
2025-04-05 21:11:21 +0200

    Cooking stuff

--- scenes/cooking/ingredient.gd
+++ scenes/cooking/ingredient.gd
@@ -7,13 +7,13 @@ signal end_drag(ingredient: Node2D)
 @export var drag_spring_force := 30000
 
 @export_category("Polygon")
-@export var min_points := 3
-@export var max_points := 8
-@export var min_radius := 20
-@export var max_radius := 40
+@export var min_angle_step := TAU*0.05
+@export var max_angle_step := TAU*0.2
+@export var min_radius := 10
+@export var max_radius := 60
 
 @onready var polygon: Polygon2D = $Polygon2D
-@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var collision_shape: CollisionPolygon2D = $CollisionPolygon2D
 
 var dragged := false
 var drag_offset := Vector2.ZERO
@@ -28,23 +28,40 @@ func _process(delta):
 
 func _get_random_shape():
 	var points = []
-	var nb_points = _rng.randi_range(min_points, max_points)
 
 	var angles = []
-	for i in range(nb_points):
-		angles.append(_rng.randf_range(0, TAU))
+	var angle = 0
+	while angle <= TAU - min_angle_step - max_angle_step:
+		angles.append(angle)
+
+		var step = _rng.randf_range(min_angle_step, max_angle_step)
+		angle += step
+
 	angles.sort()
 
-	for i in range(nb_points):
+	for i in range(angles.size()):
 		var rad = _rng.randf_range(min_radius, max_radius)
 		points.append(Vector2.from_angle(angles[i]) * rad)
 
-	return PackedVector2Array(points)
+	return points
 
 func _generate_shape():
-	var shape = _get_random_shape()
-	polygon.polygon = shape
-	collision_shape.shape.points = shape
+	var points = _get_random_shape()
+	polygon.polygon = PackedVector2Array(points)
+	collision_shape.polygon = PackedVector2Array(points)
+
+
+func _convert_shape_to_concave(points):
+	if points.is_empty():
+		return []
+
+	var out = [points[0]]
+	for i in range(1, points.size()-1):
+		out.append(points[i])
+		out.append(points[i+1])	
+	out.append(points[points.size()-1])
+
+	return out
 
 
 func _physics_process(delta):
