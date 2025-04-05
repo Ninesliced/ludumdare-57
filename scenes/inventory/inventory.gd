41c947cd906e253f42396390f05fc20be5c4e8c8
notgoyome <guillaume1.tran@epitech.eu>
2025-04-05 21:51:19 +0200

    FIX issue added logo

--- scenes/inventory/inventory.gd
+++ scenes/inventory/inventory.gd
@@ -2,13 +2,19 @@ extends Resource
 class_name Inventory
 const NB_MINERAL = 5
 
-var ressource = [0,0,0,0,0]
-
-enum
+enum Minerals 
 {
     RUBY = 0,
     EMERALD = 1,
     TOPAZ = 2,
     DIAMOND = 3,
     AMETHYST = 4
+}
+
+@export var minerals: Dictionary[Minerals, int] = {
+    Minerals.RUBY: 0,
+    Minerals.EMERALD: 0,
+    Minerals.TOPAZ: 0,
+    Minerals.DIAMOND: 0,
+    Minerals.AMETHYST: 0
 }
