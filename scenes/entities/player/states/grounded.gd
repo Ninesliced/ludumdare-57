044074d3a51c45c20f245cef8d0a80f240a10367
Yolwoocle <leo.bernard2015@hotmail.com>
2025-04-06 12:46:08 +0200

    Player animation

--- scenes/entities/player/states/grounded.gd
+++ scenes/entities/player/states/grounded.gd
@@ -5,6 +5,13 @@ class_name GroundedState
 @export var player : Player = null
 @export var playerGroundMovement : PlayerGroundMovement = null
 @export var staminaComponent : StaminaComponent = null
+@export var sprite: AnimatedSprite2D = null
+
+@export_category("Animation")
+@export var min_run_speed_animation = 40.0
+
+var look_direction = false
+
 func enter() -> void:
 	entity.collision_mask = 3
 
@@ -12,6 +19,8 @@ func process(delta: float) -> void:
 	staminaComponent.recover_stamina(staminaComponent.stamina_recovery_rate * delta)
 
 func physic_process(delta: float) -> void:
+	_update_sprite()
+
 	playerGroundMovement.physics_process(delta)
 
 	if player.burrow_requested and \
@@ -21,3 +30,17 @@ func physic_process(delta: float) -> void:
 		staminaComponent.consume_stamina(staminaComponent.stamina_on_use_cost)
 		emit_signal("state_finished", self, "burrowed")
 		return
+
+
+func _update_sprite():
+	if abs(player.velocity.x) > min_run_speed_animation:
+		look_direction = player.velocity.x < 0
+		sprite.flip_h = look_direction
+	
+	if player.is_on_floor():
+		if abs(player.velocity.x) > min_run_speed_animation:
+			sprite.play("walk")
+		else:
+			sprite.play("idle")
+	else:
+		sprite.play("jump")
